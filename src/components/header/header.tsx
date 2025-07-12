b19191722fad05dc0e4bcd0173432d9aa2f309b0
Julius <julius.santos.camster132@gmail.com>
2025-07-12 11:51:13 +0800

    adjust header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,43 +1,74 @@
-'use client';
-import React from 'react';
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import InputBase from '@mui/material/InputBase';
-import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
-import { styled, alpha } from '@mui/material/styles';
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 1),
-  marginLeft: 'auto',
+  borderRadius: '10px',
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
   width: '100%',
-  maxWidth: 400,
+  maxWidth: '600px',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: '30%',
+  },
+  [theme.breakpoints.down('sm')]: {
+    width: '100%',
+    maxWidth: '100%',
+  }
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
   display: 'flex',
   alignItems: 'center',
-  padding: '2px 8px',
+  justifyContent: 'center',
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: '#000',
+  color: 'inherit',
   width: '100%',
-  marginLeft: theme.spacing(1),
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
 }));
 
-const Header: React.FC = () => {
+export default function SearchAppBar() {
   return (
-    <AppBar position="static" sx={{ backgroundColor: '#3b4a89' }}>
-      <Toolbar>
-        <Search>
-          <StyledInputBase placeholder="Search here..." inputProps={{ 'aria-label': 'search' }} />
-          <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-            <SearchIcon />
-          </IconButton>
-        </Search>
-      </Toolbar>
-    </AppBar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+            <Box sx={{ flexGrow: 1 }} />
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
-};
-
-export default Header;
+}
